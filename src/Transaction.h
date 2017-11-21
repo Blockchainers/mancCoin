40cb03e5701e96ebeb934ab3a20fbe8bda3be019
Cameron Allan <cameron.allan@student.manchester.ac.uk>
2017-11-21 20:36:48 +0000

    Fixed to make members public

--- src/Transaction.h
+++ src/Transaction.h
@@ -2,11 +2,12 @@
 #include <string>
 
 class Transaction {
-    double index;
-    double timestamp;
-    std::vector<Transaction> transactions;
-    double proof;
-    std::string previousHash;
+    public:
+        double index;
+        double timestamp;
+        std::vector<Transaction> transactions;
+        double proof;
+        std::string previousHash;
 
     Transaction();
     ~Transaction();
