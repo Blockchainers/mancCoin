424c9ef21986c1362e20f36e3a63206578a6866e
Nathan Dane <nathan.dane@pushdr.com>
2017-12-21 15:05:08 +0000

    Enable entire project to bind RPC functions

--- src/rpc/rpc.cpp
+++ src/rpc/rpc.cpp
@@ -3,21 +3,37 @@
 #include <iostream>
 
 #include "rpc/server.h"
+#include "rpc/client.h"
 
 namespace network {
 
-  static const std::string ManccoinVersion = "1.0.0";
+  static const std::string ManccoinRPCVersion = "1.0.0";
 
   static rpc::server srv(8080);
+
+  std::string version() {
+    return ManccoinRPCVersion;
+  }
+
+  void bindRPCFunctions() {
+    // Bind RPC functions to RPC server
+    srv.bind("version", version);
+  }
+
+  template <typename F> void bind(std::string const &name, F func) {
+    srv.bind(name, func);
+  }
 
   void start() {
     std::cout << "Starting RPC server" << std::endl;
 
-    srv.bind("version", [](void) {
-      return ManccoinVersion;
-    });
-
+    bindRPCFunctions();
     srv.async_run();
+
+    // Test connection
+    rpc::client client("localhost", 8080);
+    std::string version = client.call("version").as<std::string>();
+    std::cout << "Manccoin RPC version: " << version << std::endl;
   }
 
 }
--- src/rpc/rpc.h
+++ src/rpc/rpc.h
@@ -1,8 +1,12 @@
 #ifndef RPC_H
 #define RPC_H
 
+#include <string>
 namespace network {
   void start();
+
+  template <typename F> 
+  void bind(std::string const &name, F func);
 }
 
 #endif
